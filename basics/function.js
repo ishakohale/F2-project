78d3aaeff438d26fe468e6b3c2a5aa9ca2b80ce9
ishakohale <ishapkohale@gmail.com>
2023-01-15 00:00:32 +0530

    pqr

---
+++ basics/function.js
@@ -0,0 +1,11 @@
+function makemaggie(maggie, water, masala) {
+
+    console.log("Your maggie will be ready in " +
+        maggie * 2 +
+        "minutes" +
+        "and we have used " +
+        maggie + "packets of maggie" +
+        water + "glass of water" +
+        masala + "spice masala")
+}
+makemaggie(4, 2, 1);
